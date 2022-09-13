f8b07242aa13d7f3b26853b0619a65969f5e63c6
Will Briggs <wbriggs90@gmail.com>
2022-09-12 19:05:23 -0600

    Modified script to pass a config file to create a league object

--- BFL_Script.py
+++ BFL_Script.py
@@ -8,22 +8,18 @@ Created on Wed Sep  9 20:59:33 2020
 """
 
 import RTSLeague as rt
+import configparser as cp
 
-LID ='6779'
-UID = 'a99e678d379292511bf10cb24dd93fb'
-X = '922249'
-TID = '4996321'
-REALTIME = '848795-5edcae50b62a-f0efe4e66ecf529a01c8b6e642848ab07e2caaa84ec97da17e1b'
+config = cp.ConfigParser()
 
-league = rt.privateLeague(LID,UID,X,REALTIME,2022, 'Your Team Name')
+config.read("config_private.ini")
+
+
+
+league = rt.privateLeague(config)
 print()
 
 for slot in league.slotvalues:
     print("positional Analysis for ", slot)
-    if slot !='KICKER' and slot !='DEF':
-        print(league.positionalAnalysis((slot)))
-        print()
-    else:
-        print("No check Run")
-    
+ 
 league.saveAllData("data.csv")
